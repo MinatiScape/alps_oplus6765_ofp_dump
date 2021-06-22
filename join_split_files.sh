3b5eb12cb69202e8e37aaddc47c94a39735a5650
root <root@fv-az54-974.qczjibyfz4dularrisj3jz1gre.bx.internal.cloudapp.net>
2021-06-22 10:51:31 +0000

    Add extras for sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 1609743540423 release-keys
    Signed-off-by: root <root@fv-az54-974.qczjibyfz4dularrisj3jz1gre.bx.internal.cloudapp.net>

---
+++ join_split_files.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat my_stock/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null >> my_stock/priv-app/OppoGallery2/OppoGallery2.apk
+rm -f my_stock/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null
+cat my_stock/del-app/OppoVideoEditor/OppoVideoEditor.apk.* 2>/dev/null >> my_stock/del-app/OppoVideoEditor/OppoVideoEditor.apk
+rm -f my_stock/del-app/OppoVideoEditor/OppoVideoEditor.apk.* 2>/dev/null
+cat system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null >> system/system/app/OppoCamera/OppoCamera.apk
+rm -f system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/system_ext/priv-app/SystemUI/SystemUI.apk.* 2>/dev/null >> system/system/system_ext/priv-app/SystemUI/SystemUI.apk
+rm -f system/system/system_ext/priv-app/SystemUI/SystemUI.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/system_ext/app/OppoEngineerCamera/OppoEngineerCamera.apk.* 2>/dev/null >> system/system/system_ext/app/OppoEngineerCamera/OppoEngineerCamera.apk
+rm -f system/system/system_ext/app/OppoEngineerCamera/OppoEngineerCamera.apk.* 2>/dev/null
+cat my_heytap/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> my_heytap/priv-app/GmsCore/GmsCore.apk
+rm -f my_heytap/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat my_heytap/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> my_heytap/priv-app/Velvet/Velvet.apk
+rm -f my_heytap/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat my_heytap/app/Maps/Maps.apk.* 2>/dev/null >> my_heytap/app/Maps/Maps.apk
+rm -f my_heytap/app/Maps/Maps.apk.* 2>/dev/null
+cat my_heytap/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> my_heytap/app/WebViewGoogle/WebViewGoogle.apk
+rm -f my_heytap/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat my_heytap/app/YouTube/YouTube.apk.* 2>/dev/null >> my_heytap/app/YouTube/YouTube.apk
+rm -f my_heytap/app/YouTube/YouTube.apk.* 2>/dev/null
+cat my_heytap/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> my_heytap/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f my_heytap/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
